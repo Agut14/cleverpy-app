493d20b6d4707307a0bbd9f99d03a2242add9548
Carlos Agut <carlosagutvives@gmail.com>
2022-10-04 01:24:15 +0200

    first test

---
+++ src/tests/hooks/useForm.test.ts
@@ -0,0 +1,30 @@
+import { renderHook } from "@testing-library/react";
+import { useForm } from "../../hooks/useForm";
+
+export{};
+
+describe('Pruebas en el useForm', () => { 
+
+    const initialForm = {
+        title: 'Hola Mundo',
+        body: 'Gracias por la oportunidad'
+    }
+
+    test('regresa el objeto por defecto', () => {
+
+        const { result } = renderHook(() => useForm( initialForm ));
+        console.log( result.current)
+        expect( result.current ).toEqual({
+            title: initialForm.title,
+            body: initialForm.body,
+            setFormData: expect.any( Function ),
+            onHandleChange: expect.any( Function ),
+            resetData: expect.any( Function ),
+            isErrorState: false,
+            sendData: expect.any( Function ),
+            toggleForm: expect.any( Function ),
+            isDisabled: true
+          });
+ 
+    })
+ })
